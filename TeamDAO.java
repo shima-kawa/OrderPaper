a233db37268854446bc30182389d23f177976a09
yu-kawa <shimashima2715246desu@gmail.com>
2023-09-15 17:18:34 +0900

    fix: SQLインジェクション対策の実装

--- TeamDAO.java
+++ TeamDAO.java
@@ -54,17 +54,18 @@ public class TeamDAO {
     }
 
     public TeamDTO selectOneTeam(int id){
-        Statement stmt = null;
+        PreparedStatement ps = null;
         ResultSet rs = null;
         String[] players = new String[8];
         TeamDTO tdto = new TeamDTO();
-        String sql = "SELECT * FROM teams WHERE id = " + id;
+        String sql = "SELECT * FROM teams WHERE id = ?";
         try{
             connect();
             // 2.ステートメントを生成
-            stmt = con.createStatement();
+            ps = con.prepareStatement(sql);
             // 3.SQLを実行
-            rs = stmt.executeQuery(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
             // 4.検索結果の処理
             while(rs.next()){
                 TeamBean tb = new TeamBean();
@@ -81,7 +82,7 @@ public class TeamDAO {
         } finally{
             try{
                 if(rs != null) rs.close();
-                if(stmt != null) stmt.close();
+                if(ps != null) ps.close();
             } catch(Exception e){
                 e.printStackTrace();
             }
@@ -91,17 +92,18 @@ public class TeamDAO {
     }
 
     public TeamDTO selectOneTeam(String name){
-        Statement stmt = null;
+        PreparedStatement pstmt = null;
         ResultSet rs = null;
         String[] players = new String[8];
         TeamDTO tdto = new TeamDTO();
-        String sql = "SELECT * FROM teams WHERE name = '" + name + "'";
+        String sql = "SELECT * FROM teams WHERE name = ?";
         try{
             connect();
             // 2.ステートメントを生成
-            stmt = con.createStatement();
+            pstmt = con.prepareStatement(sql);
             // 3.SQLを実行
-            rs = stmt.executeQuery(sql);
+            pstmt.setString(1, name);
+            rs = pstmt.executeQuery();
             // 4.検索結果の処理
             while(rs.next()){
                 TeamBean tb = new TeamBean();
@@ -118,7 +120,7 @@ public class TeamDAO {
         } finally{
             try{
                 if(rs != null) rs.close();
-                if(stmt != null) stmt.close();
+                if(pstmt != null) pstmt.close();
             } catch(Exception e){
                 e.printStackTrace();
             }
@@ -127,27 +129,71 @@ public class TeamDAO {
         return tdto;
     }
     public int insert(int id, String name, String[] players){
-        String sql = "INSERT INTO teams VALUES (" + id + ", '" + name + "'";
-        for(int i=0; i<players.length; i++){
-            sql = sql + ", '" + players[i] + "'";
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        String sql = "INSERT INTO teams VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try{
+            connect();
+            // ステートメントを生成
+            pstmt = con.prepareStatement(sql);
+            // SQL実行
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            for(int i=0; i<players.length; i++){
+                pstmt.setString(3+i, players[i]);
+            }
+            result = pstmt.executeUpdate();
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            try{
+                if(pstmt != null) pstmt.close();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
         }
-        sql = sql + ")";
-        return executeSql(sql);
+        disconnect();
+        return result;
     }
 
     public int update(int id, String name, String[] players){
-        String sql = "UPDATE teams SET id = " + id + ", name = '" + name + "'";
-        for(int i=0; i<players.length; i++){
-            sql = sql + ", player" + (i+1) + " = '" + players[i] + "'";
+        String sql = "UPDATE teams SET id=?, name = ?, player1=?, player2=?, player3=?, player4=?, player5=?, player6=?, player7=?, player8=? WHERE id = ?";
+        PreparedStatement pstmt = null;
+        int result = 0;
+
+        try {
+            connect();
+            // ステートメントを生成
+            pstmt = con.prepareStatement(sql);
+            // SQL実行
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            for(int i=0; i<players.length; i++){
+                pstmt.setString(3+i, players[i]);
+            }
+            pstmt.setInt(11, id);
+            result = pstmt.executeUpdate();
+        } catch (Exception e) {
+            e.getStackTrace();
+        } finally{
+            try {
+                if(pstmt != null) pstmt.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        sql = sql + " WHERE id = " + id;
-        return executeSql(sql);
+        disconnect();
+        return result;
     }
 
+    // preparedStatementにする？
     public int delete(int id){
         String sql = "DELETE FROM teams WHERE id = " + id;
         return executeSql(sql);
     }
+
     public int executeSql(String sql){
         Statement stmt = null;
         int result = 0;
